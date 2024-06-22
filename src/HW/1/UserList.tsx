bf1c5a0e7e4b41797fefef00400d503b5fc6b19b
Raevskaya Natalie <ya.brokkoli@gmail.com>
2024-06-22 23:56:50 +0300

    refactor HW1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,29 +1,34 @@
-type AddressType = {
-  street: any; // ПОДПРАВЛЯЕМ any
-  city: any; // ПОДПРАВЛЯЕМ any
+export type AddressType = {
+    street: string
+    city: string
 };
 
-type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
+export type UserType = {
+    id: number
+    name: string
+    age: number
+    address: AddressType
 };
 
-type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+export type UserListPropsType = {
+    users: UserType[]
 };
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
-  return (
-    <div id={'hw01-users'}>
-      <h2>User List:</h2>
+export const UserList = ({ users }: UserListPropsType) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2> User List:</h2>
 
-      {/*<ul>*/}
-      {/*  {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...*/}
-      {/*    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>*/}
-      {/*      <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>*/}
-      {/*      {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
-    </div>
-  );
+            <ul>
+                {users.map((user) => {
+                    return (
+                        <li key={user.id} id={`hw01-user-${user.id}`}>
+                            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                            {user.address.street}, {user.address.city}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
